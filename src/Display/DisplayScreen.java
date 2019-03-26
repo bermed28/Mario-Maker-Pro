c4e52576a59df1396deec8f58f19fb8e8ac54483
AlexVR <josian.velez@upr.edu>
2019-03-26 15:17:54 -0400

    name and icon

--- src/Display/DisplayScreen.java
+++ src/Display/DisplayScreen.java
@@ -41,7 +41,7 @@ public class DisplayScreen {
         frame.setBackground(Color.black);
 
         try {
-            frame.setIconImage(ImageIO.read(new File("res/Sheets/icon.png")));
+            frame.setIconImage(ImageIO.read(new File("res/Sheets/item/SL1.png")));
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/Main/Launch.java
+++ src/Main/Launch.java
@@ -9,7 +9,7 @@ public class Launch {
 
     public static void main(String[] args) {
         Handler handler = new Handler();
-        GameSetUp game = new GameSetUp("PA3",handler);
+        GameSetUp game = new GameSetUp("Mario Maker Pro TM. (CR Nintendo)",handler);
         handler.setGame(game);
         game.start();
     }
